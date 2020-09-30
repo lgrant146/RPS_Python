d95d8c4f96c7ca133bc36c0386914dcbf502174b
Leroy Grant <lgrant146@gmail.com>
2020-09-29 23:32:18 -0500

    rock paper scissors game using random

--- Rock-Paper-Scissors/task/rps/game.py
+++ Rock-Paper-Scissors/task/rps/game.py
@@ -1,9 +1,24 @@
 # Write your code here
+import random
+
 choice = input()
 
-if choice == "paper":
-    print("Sorry, but the computer chose scissors")
-if choice == "rock":
-    print("Sorry, but the computer chose paper")
-if choice == "scissors":
-    print("Sorry, but the computer chose rock")
+rps = ['rock', 'paper', 'scissors']
+
+computer_choice = random.choice(rps)
+
+win = {'rock': 'scissors',
+       'scissors': 'paper',
+       'paper': 'rock'
+       }
+lose = {'rock': 'paper',
+        'scissors': 'rock',
+        'paper': 'scissors'
+        }
+
+if choice == computer_choice:
+    print("There is a draw (" + computer_choice + ")")
+elif win[choice] == computer_choice:
+    print("Well done. The computer chose " + computer_choice + " and failed")
+elif lose[choice] == computer_choice:
+    print(f"Sorry, but the computer chose {computer_choice}")
